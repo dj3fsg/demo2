41b32892ccb5cff62a9794193c0707b7b0f1f94b
Satoshi Ishii <121612716+dj3fsg@users.noreply.github.com>
2024-11-01 23:45:15 +0900

    認可機能の実装

--- src/main/java/com/example/demo2/AdminPageController.java
+++ src/main/java/com/example/demo2/AdminPageController.java
@@ -1,5 +1,6 @@
 package com.example.demo2;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminPageController {
 	@GetMapping("/adminPage")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public String adminPage() {
 		return "adminPage";
 	}
